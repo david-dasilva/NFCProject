938654e45e9dddff72b5dd4771eb5056ceeb73c0
FurqX <chatton.jeremy@gmail.com>
2013-10-23 13:58:37 +0200

    Ecriture sur un tag NFC

--- src/org/mbds/android/tagnfc/NFCReader.java
+++ src/org/mbds/android/tagnfc/NFCReader.java
@@ -1,5 +1,6 @@
 package org.mbds.android.tagnfc;
 
+import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.Arrays;
 
@@ -8,15 +9,19 @@ import android.app.Activity;
 import android.app.PendingIntent;
 import android.app.ProgressDialog;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.IntentFilter.MalformedMimeTypeException;
 import android.net.Uri;
+import android.nfc.FormatException;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
+import android.nfc.TagLostException;
 import android.nfc.tech.Ndef;
+import android.nfc.tech.NdefFormatable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Parcelable;
@@ -202,5 +207,85 @@ public class NFCReader extends Activity {
 			return true;
 		}
 		return super.onKeyDown(keyCode, event);
+	}
+	
+	public static boolean writeTag(Context context, Tag tag, String data) {     
+	    // Record to launch Play Store if app is not installed
+	    NdefRecord appRecord = NdefRecord.createApplicationRecord(context.getPackageName());
+	 
+	    // Record with actual data we care about
+	    NdefRecord relayRecord = new NdefRecord(NdefRecord.TNF_MIME_MEDIA,
+	                                            new String("application/" + context.getPackageName()).getBytes(Charset.forName("US-ASCII")),
+	                                            null, data.getBytes());
+	 
+	    // Complete NDEF message with both records
+	    NdefMessage message = new NdefMessage(new NdefRecord[] {relayRecord, appRecord});
+	 
+	    try {
+	        // If the tag is already formatted, just write the message to it
+	        Ndef ndef = Ndef.get(tag);
+	        if(ndef != null) {
+	            ndef.connect();
+	 
+	            // Make sure the tag is writable
+	            if(!ndef.isWritable()) {
+	                //DialogUtils.displayErrorDialog(context, R.string.nfcReadOnlyErrorTitle, R.string.nfcReadOnlyError);
+	                return false;
+	            }
+	 
+	            // Check if there's enough space on the tag for the message
+	            int size = message.toByteArray().length;
+	            if(ndef.getMaxSize() < size) {
+	                //DialogUtils.displayErrorDialog(context, R.string.nfcBadSpaceErrorTitle, R.string.nfcBadSpaceError);
+	                return false;
+	            }
+	 
+	            try {
+	                // Write the data to the tag
+	                ndef.writeNdefMessage(message);
+	 
+	                //DialogUtils.displayInfoDialog(context, R.string.nfcWrittenTitle, R.string.nfcWritten);
+	                return true;
+	            } catch (TagLostException tle) {
+	                //DialogUtils.displayErrorDialog(context, R.string.nfcTagLostErrorTitle, R.string.nfcTagLostError);
+	                return false;
+	            } catch (IOException ioe) {
+	                //DialogUtils.displayErrorDialog(context, R.string.nfcFormattingErrorTitle, R.string.nfcFormattingError);
+	                return false;
+	            } catch (FormatException fe) {
+	                //DialogUtils.displayErrorDialog(context, R.string.nfcFormattingErrorTitle, R.string.nfcFormattingError);
+	                return false;
+	            }
+	        // If the tag is not formatted, format it with the message
+	        } else {
+	            NdefFormatable format = NdefFormatable.get(tag);
+	            if(format != null) {
+	                try {
+	                    format.connect();
+	                    format.format(message);
+	 
+	                    //DialogUtils.displayInfoDialog(context, R.string.nfcWrittenTitle, R.string.nfcWritten);
+	                    return true;
+	                } catch (TagLostException tle) {
+	                    //DialogUtils.displayErrorDialog(context, R.string.nfcTagLostErrorTitle, R.string.nfcTagLostError);
+	                    return false;
+	                } catch (IOException ioe) {
+	                    //DialogUtils.displayErrorDialog(context, R.string.nfcFormattingErrorTitle, R.string.nfcFormattingError);
+	                    return false;
+	                } catch (FormatException fe) {
+	                    //DialogUtils.displayErrorDialog(context, R.string.nfcFormattingErrorTitle, R.string.nfcFormattingError);
+	                    return false;
+	                }
+	            } else {
+	                //DialogUtils.displayErrorDialog(context, R.string.nfcNoNdefErrorTitle, R.string.nfcNoNdefError);
+	                return false;
+	            }
+	        }
+	    } catch(Exception e) {
+	        //DialogUtils.displayErrorDialog(context, R.string.nfcUnknownErrorTitle, R.string.nfcUnknownError);
+	    	
+	    }
+	 
+	    return false;
 	}
 }
--- src/org/mbds/android/tagnfc/TagActivity.java
+++ src/org/mbds/android/tagnfc/TagActivity.java
@@ -1,8 +1,12 @@
 package org.mbds.android.tagnfc;
 
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -21,11 +25,37 @@ public class TagActivity extends Activity {
         btnShare.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                Share();
+                
+            	// Construct the data to write to the tag
+            	// Should be of the form [relay/group]-[rid/gid]-[cmd]
+            	String nfcMessage = NFCReader.MESSAGE+"/toto";
+            	 
+            	// When an NFC tag comes into range, call the main activity which handles writing the data to the tag
+            	NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(getBaseContext());
+            	 
+            	Intent nfcIntent = new Intent(getBaseContext(), TagActivity.class).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            	nfcIntent.putExtra("nfcMessage", nfcMessage);
+            	PendingIntent pi = PendingIntent.getActivity(getBaseContext(), 0, nfcIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+            	IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);  
+            	 
+            	nfcAdapter.enableForegroundDispatch((Activity)getBaseContext(), pi, new IntentFilter[] {tagDetected}, null);
+           
             }
         });
 		
 	}
+	
+	public void onNewIntent(Intent intent) {
+	    // When an NFC tag is being written, call the write tag function when an intent is
+	    // received that says the tag is within range of the device and ready to be written to
+	    Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+	    String nfcMessage = intent.getStringExtra("nfcMessage");
+	 
+	    if(nfcMessage != null) {
+	        NFCReader.writeTag(this, tag, nfcMessage);
+	    }
+	}
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
